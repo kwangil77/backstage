4b6c5fed0707c45d3cc383f1401958948a85ea06
kwangil.ha <kwangil.ha@musinsa.com>
2024-07-01 10:26:06 +0900

    feat: Backstage Keycloak OIDC 연동

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -43,7 +43,7 @@ export const apis: AnyApiFactory[] = [
           icon: () => null,
         },
         environment: configApi.getOptionalString('auth.environment'),
-        defaultScopes: ['openid', 'email,', 'profile,', 'groups'],
+        defaultScopes: ['openid'],
       }),
   }),
   createApiFactory(
